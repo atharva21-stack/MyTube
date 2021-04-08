9ff7b0501ecbc7f56d61c634941e8b02f4202526
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-04-08 23:13:03 +0530

    Update RecommendedVideo component

---
+++ my-app/src/RecommendedVideos.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import "./RecommendedVideos.css"
+import VideoCard from "./VideoCard"
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedvideos">
+            <h2>Recommended</h2>
+            <div className="recommendedvideos_Videos">
+                <VideoCard
+                title = "Sony PSVR 2 – THIS is it!"
+                    views= "1.2K views"
+                    timestamp="18 minutes ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnigRClUaCknAkpuOLRPHy9Z5J_7TsZLVbYoWYqeOw=s68-c-k-c0x00ffffff-no-rj"
+                    channel="ZONEofTECH"
+                    image="https://i.ytimg.com/an_webp/-rege5479-A/mqdefault_6s.webp?du=3000&sqp=CMCTvIMG&rs=AOn4CLBOcTSxCvj-KSNTARE_Pcrmu28ckg"
+                 />
+                <VideoCard 
+                    title = "OnePlus Watch Unboxing..."
+                    views= "306K views"
+                    timestamp="6 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnh3QH3UYxKLoz4S9dRE9dDo_nhqE6cUZYASHYTJQQ=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Unbox Therapy"
+                    image="https://i.ytimg.com/an_webp/rsSO5iYhjTg/mqdefault_6s.webp?du=3000&sqp=CKjuu4MG&rs=AOn4CLDMPzt8ql4FD95YknMCqgSzK0oPpg"
+                />
+                <VideoCard 
+                    title = "How to assemble a GAMING CHAIR. Ant Esports 8077 review."
+                    views= "122K views"
+                    timestamp="2 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniledWYzV7RdkCKtEOKVzzxt47QLinx211Pl3TlEA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="INDIAN GAMER"
+                    image="https://i.ytimg.com/an_webp/EXs-dfJlZJA/mqdefault_6s.webp?du=3000&sqp=CKCrvIMG&rs=AOn4CLDGotE0pN05EXHMMB0dC4t97Km5JA"
+                
+                />
+                <VideoCard 
+                title = "Why Did You Kill Me? | Official Trailer | Netflix"
+                    views= "11K views"
+                    timestamp="2 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwni_LdnpDi-SOIhjp4Kxo2l_yVBoYsfdDCpUM5VDzg=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Netflix"
+                    image="https://i.ytimg.com/an_webp/QEXV8Rif8Vc/mqdefault_6s.webp?du=3000&sqp=COmpvIMG&rs=AOn4CLCB_8T5R2aUqTNny2LHMkoJFAsF4w"
+                 />
+                <VideoCard 
+                    title = "Happy 90th Birthday, Warren Buffett!"
+                    views= "1.5M views"
+                    timestamp="7 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjXQ1VH9YHQ289ZPeprXvtOi5KexAKOBvodL0jM=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Bill Gates"
+                    image="https://i.ytimg.com/an_webp/8U6ZraAj5Q4/mqdefault_6s.webp?du=3000&sqp=CMSxvIMG&rs=AOn4CLDZ4X4uTAtU24mU1bydN_CM3ZuGtA"
+                 
+                />
+                <VideoCard 
+                    title = "Iceland volcano: Drone footage captures stunning up-close view of eruption"
+                    views= "1.5M views"
+                    timestamp="2 weeks ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjXQ1VH9YHQ289ZPeprXvtOi5KexAKOBvodL0jM=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Global News"
+                    image="https://i.ytimg.com/an_webp/b9Hq6bTBF2A/mqdefault_6s.webp?du=3000&sqp=CKCyvIMG&rs=AOn4CLCHHciHXt7PdzajALiMN-q2cdbrUw"
+                 
+                />
+                <VideoCard 
+                     title = "I Am Mother | Tráiler oficial | Netflix"
+                    views= "2.9M views"
+                    timestamp="1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngLidFMPb1105eZIla3FgCk7yZ1aoP31ZV8pjM7xQ=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Netflix Latinoamérica"
+                    image="https://i.ytimg.com/an_webp/Bg_tJvCA8zw/mqdefault_6s_480x270.webp?du=3000&sqp=CJCnvIMG&rs=AOn4CLBw8cTadIT53KfKJEKxrnysI4fXkg"
+                 
+                />
+                <VideoCard
+                title = "Sony PSVR 2 – THIS is it!"
+                    views= "1.2K views"
+                    timestamp="18 minutes ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnigRClUaCknAkpuOLRPHy9Z5J_7TsZLVbYoWYqeOw=s68-c-k-c0x00ffffff-no-rj"
+                    channel="ZONEofTECH"
+                    image="https://i.ytimg.com/an_webp/-rege5479-A/mqdefault_6s.webp?du=3000&sqp=CMCTvIMG&rs=AOn4CLBOcTSxCvj-KSNTARE_Pcrmu28ckg"
+                 />
+                <VideoCard 
+                    title = "OnePlus Watch Unboxing..."
+                    views= "306K views"
+                    timestamp="6 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnh3QH3UYxKLoz4S9dRE9dDo_nhqE6cUZYASHYTJQQ=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Unbox Therapy"
+                    image="https://i.ytimg.com/an_webp/rsSO5iYhjTg/mqdefault_6s.webp?du=3000&sqp=CKjuu4MG&rs=AOn4CLDMPzt8ql4FD95YknMCqgSzK0oPpg"
+                />
+                <VideoCard 
+                    title = "How to assemble a GAMING CHAIR. Ant Esports 8077 review."
+                    views= "122K views"
+                    timestamp="2 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniledWYzV7RdkCKtEOKVzzxt47QLinx211Pl3TlEA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="INDIAN GAMER"
+                    image="https://i.ytimg.com/an_webp/EXs-dfJlZJA/mqdefault_6s.webp?du=3000&sqp=CKCrvIMG&rs=AOn4CLDGotE0pN05EXHMMB0dC4t97Km5JA"
+                
+                />
+                <VideoCard 
+                    title = "How to assemble a GAMING CHAIR. Ant Esports 8077 review."
+                    views= "122K views"
+                    timestamp="2 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniledWYzV7RdkCKtEOKVzzxt47QLinx211Pl3TlEA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="INDIAN GAMER"
+                    image="https://i.ytimg.com/an_webp/EXs-dfJlZJA/mqdefault_6s.webp?du=3000&sqp=CKCrvIMG&rs=AOn4CLDGotE0pN05EXHMMB0dC4t97Km5JA"
+                
+                />
+                <VideoCard 
+                title = "Why Did You Kill Me? | Official Trailer | Netflix"
+                    views= "11K views"
+                    timestamp="2 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwni_LdnpDi-SOIhjp4Kxo2l_yVBoYsfdDCpUM5VDzg=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Netflix"
+                    image="https://i.ytimg.com/an_webp/QEXV8Rif8Vc/mqdefault_6s.webp?du=3000&sqp=COmpvIMG&rs=AOn4CLCB_8T5R2aUqTNny2LHMkoJFAsF4w"
+                 />
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default RecommendedVideos
