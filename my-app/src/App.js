b6de9e132392c8dce3dcf89e039fc10a442409e2
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-04-09 23:04:14 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,30 @@ import Header from "./Header";
 import Sidebar from "./Sidebar"
 import RecommendedVideos from "./RecommendedVideos"
 import './App.css';
+import {BrowserRouter as Router , Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+    <Router>
     <Header/>
+      <Switch>
+      <Route path="/search/:searchTerm">
+      <h1>Search Page</h1>
+      </Route>
+      <Route path="/">
       
       <div className="app_page">
         <Sidebar />
         <RecommendedVideos />
         </div>
+      </Route>
+    </Switch>
+    </Router>
+    
+    
+   
+        
 
     
         
