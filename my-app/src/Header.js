65d83175f73443a62dd870939ed09778eddd612e
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-04-29 20:40:16 +0530

    Update Header component

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Header.css';
 import MenuIcon from "@material-ui/icons/Menu";
@@ -7,20 +7,29 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
-function header() {
+import { Link } from 'react-router-dom';
+function Header() {
+        const [inputsearch , setInputSearch] = useState('');
    
     return (
+        
         <div className="header">
             <div className="header_left">
             <MenuIcon/>
+            <Link to={'/'}>
             <img className="header_logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt="text">
-            
             </img>
+            </Link>
+            
             </div>
 
             <div className="header_input">
-            <input placeholder="Search" type="text"></input>
+            <input onChange={e => setInputSearch(e.target.value)} placeholder="Search" type="text"></input>
+            
+            <Link to={'/search/${inputSearch}'}>
             <SearchIcon className="header_inputButton" />
+            </Link>
+            
             </div>
             
             <div>
@@ -40,4 +49,4 @@ function header() {
     )
 }
 
-export default header
+export default Header
